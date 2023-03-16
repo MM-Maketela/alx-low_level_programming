1fee4fcd69459aa16c621629ec56025e16948b05
maketela mafa <maketelamafa2@gmail.com>
2023-03-16 13:42:20 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
 # include <stdio.h>
 int main(void)
 {	/*Declaration:     int puts(const char *str);*/
-	puts("\"Programming is like building 
-	a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
