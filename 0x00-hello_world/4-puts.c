b24fcebca302fa00a784a467b0dd29dd08fc3c0b
maketela mafa <maketelamafa2@gmail.com>
2023-03-16 13:00:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
+/*
+Header file:     #include <stdio.h>
+Declaration:     int puts(const char *str);
+Return:          returns a nonnegative value on success or an EOF upon failure.
+*/
 # include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
