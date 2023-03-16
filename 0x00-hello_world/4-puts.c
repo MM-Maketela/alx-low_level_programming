d0ecdb6c588846c146870914f68d04d16a32c2ad
maketela mafa <maketelamafa2@gmail.com>
2023-03-16 13:55:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,12 @@
  *Return:          returns a nonnegative value on success or an EOF upon failure.
 */
 # include <stdio.h>
+# include <string.h>
 int main(void)
 {	/*Declaration:     int puts(const char *str);*/
 	char str1[] = "\"Programming is like building ";
 	char str2[] = "a multilingual puzzle";
-	puts(strcat(str1,str2));
+	strcat(str1,str2);
+	puts(str1);
 	return (0);
 }
