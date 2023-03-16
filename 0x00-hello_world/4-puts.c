8b122b62a80abbd20944c684a0d2f254dbea4804
maketela mafa <maketelamafa2@gmail.com>
2023-03-16 13:11:06 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 /*
-*Header file:     #include <stdio.h>
-*Declaration:     int puts(const char *str);
-*Return:          returns a nonnegative value on success or an EOF upon failure.
+ * description
+ *Header file:     #include <stdio.h>
+ *Declaration:     int puts(const char *str);
+ *Return:          returns a nonnegative value on success or an EOF upon failure.
 */
 # include <stdio.h>
 int main(void)
