4f54d70bdb9c5bb365933721b469b704ab9dca6c
maketela mafa <maketelamafa2@gmail.com>
2023-03-17 03:08:59 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: a C program that prints a string to the standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
