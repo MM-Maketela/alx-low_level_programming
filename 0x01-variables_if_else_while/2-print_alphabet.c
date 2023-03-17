047b32fbb774ba9444c442b3dd8780ef316266ed
maketela mafa <maketelamafa2@gmail.com>
2023-03-17 09:53:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints the charactors ofthe alphabet.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	char string[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(string[i]);
+		putchar('\n');
+	}
+}
