b69d9fdc2c20fd6337afa0c91be870fb79977d86
maketela mafa <maketelamafa2@gmail.com>
2023-03-17 01:54:56 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int number;
-char charector;
-char string[];
-long longNumber;
-long long longLongNumber;
-float decimalNumber;
-printf("Size of a char: %c\n", sizeof(charector));
-printf("Size of an int: %d\n", sizeof(number));
-printf("Size of a long int: %lu\n", sizeof(longNumber));
-printf("Size of a long long int: %llu\n", sizeof(longLongNumber));
-printf("Size of a float: %f\n", sizeof(decimalNumber));
+int number = 5;
+char charector = '5';
+char string[5] = "hello";
+long longNumber = 5;
+long long longLongNumber = 5;
+float decimalNumber = 5.1f;
+printf("Size of a char: %ld\n", sizeof(charector));
+printf("Size of an int: %ld\n", sizeof(number));
+printf("Size of a long int: %ld\n", sizeof(longNumber));
+printf("Size of a long long int: %ld\n", sizeof(longLongNumber));
+printf("Size of a float: %ld\n", sizeof(decimalNumber));
 return (0);
 }
