08576bfff400e64dbe537e3b3aed98f2c06f4583
maketela mafa <maketelamafa2@gmail.com>
2023-03-17 10:40:27 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints numbers of base 10 from 0.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char numbers[] = "0123456789";
+
+	printf("%s\n", numbers);
+	return (0);
+}
