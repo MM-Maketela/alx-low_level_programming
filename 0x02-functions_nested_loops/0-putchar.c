1fda73051f6d6eb3b587d0de4456df9637995e4b
maketela mafa <maketelamafa2@gmail.com>
2023-03-21 04:22:26 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints _putchar.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "_putchar");
+	return (0);
+}
