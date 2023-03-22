4021b5de2bdbbf828858ed8e195908557b1b3e29
maketela mafa <maketelamafa2@gmail.com>
2023-03-22 04:37:37 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: A program that prints the letters a-z;
- * Return: 0 (success)
+ * print_alphabet - entry point
+ * Description: a program that prints out the letter of alphabet a-z.
+ * Return: void(success)
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alphabets[i]);
+	}
+	_putchar('\n');
 }
