5e28ef2a7994478d87ef2a2ffc3c16c4e6e7bafb
maketela mafa <maketelamafa2@gmail.com>
2023-03-22 03:30:03 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	char string[] = "_putchar";
+	char string[9] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(string[i]);
 	}
+	_putchar('\n');
 	return (0);
 }
