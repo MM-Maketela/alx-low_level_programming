9f932012e6bc1e362fd6adffb053f5eac39a2793
maketela mafa <maketelamafa2@gmail.com>
2023-03-22 09:31:58 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * Description: checks the sign of n.
+ * @n: takes a paremeter of type int.
+ * Return: 1 (if n is positiv) 0 (if n is zero) and -1 othewise.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+
+}
