21235dc935be94877bed4a170fe517155f8882ef
maketela mafa <maketelamafa2@gmail.com>
2023-03-27 10:26:20 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * Description: this function updates the value of the pointed value;
+ * @n: pointer pointing to an integer.
+ * Return: void;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h>
+#include <unistd.h>
+void reset_to_98(int *n);
