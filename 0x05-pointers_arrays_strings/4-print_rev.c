8d0157d864703a2666aa0ab2b837824fdf92c10d
maketela mafa <maketelamafa2@gmail.com>
2023-03-27 14:09:29 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * Description: prints a string in revers.
+ * @s: pointer pointing to strng.
+ * Return: void;
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+
+	while (count >= 0)
+	{
+		_putchar(*s);
+		count--;
+		s--;
+	}
+	_putchar('\n');
+}
