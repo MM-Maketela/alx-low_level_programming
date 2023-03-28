96c5986229cde1eb6ed6b367ebcad15216ee5879
maketela mafa <maketelamafa2@gmail.com>
2023-03-28 08:56:50 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * Description: prints every other char of a string.
+ * @str: pointer pointing to strng.
+ * Return: void;
+ */
+void puts2(char *str)
+{
+	int strlen;
+	int index;
+
+	strlen = 0;
+
+	while (str[strlen] != '\0')
+	{
+		strlen++;
+	}
+	for (index = 0; index < strlen; index +=2)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
