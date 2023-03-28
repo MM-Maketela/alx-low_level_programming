9588b9feb598b997f619d13909c86e85ff5def91
maketela mafa <maketelamafa2@gmail.com>
2023-03-28 08:36:32 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - Entry point
+ * Description: prints a string in revers.
+ * @s: pointer pointing to strng.
+ * Return: void;
+ */
+void rev_string(char *s)
+{
+	int count;
+	char tempChar;
+	int index; 
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	for (index = count - 1; index >= count / 2; index-- )
+	{
+		tempChar = s[index];
+		s[index] = s[count - index - 1];
+		s[count - index - 1] = tempChar;
+	}
+}
