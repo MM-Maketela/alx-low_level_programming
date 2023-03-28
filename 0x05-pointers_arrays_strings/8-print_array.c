c49f0adf6f03af670d17eb937734a8547be53f63
maketela mafa <maketelamafa2@gmail.com>
2023-03-28 09:36:13 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,10 @@ void print_array(int *a, int n)
 
 	for (index = 0; index < n; index++)
 	{
-		printf("%d;\n", a[index]);
+		if (index != n - 1)
+			printf("%d, ", a[index]);
+		else
+			printf("%d", a[index]);
 	}
+	printf("\n");
 }
